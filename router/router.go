37567a62d57590399739ed64197aa90f97671743
to4to <justjustifying@outlook.com>
2024-05-31 23:39:23 +0530

    feat: Add API endpoint for creating new stock

--- router/router.go
+++ router/router.go
@@ -13,6 +13,7 @@ func Router()*mux.Router{
 
 	router.HandleFunc("api/stock/{id}",middleware.GetStock).Methods("GET","OPTIONS")
 router.HandleFunc("api/stock", middleware.GetAllStock).Methods("GET", "OPTIONS")
+router.HandleFunc("api/newstock", middleware.CreateStock).Methods("POST", "OPTIONS")
 
 return router
 }
